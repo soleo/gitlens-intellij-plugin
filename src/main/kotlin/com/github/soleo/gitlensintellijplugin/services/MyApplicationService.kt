c362ff9a273e0e6188580f9c8c66996a9220656f
GitHub Action <action@github.com>
2021-05-13 21:59:09 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/soleo/gitlensintellijplugin/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.soleo.gitlensintellijplugin.services
+
+import com.github.soleo.gitlensintellijplugin.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/soleo/gitlensintellijplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/soleo/gitlensintellijplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.soleo.gitlensintellijplugin.services
 
+import com.github.soleo.gitlensintellijplugin.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
